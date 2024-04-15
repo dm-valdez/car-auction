58274e59a0e2e651c5838ec917bcd5c5377d528c
dm-valdez <dmvdev.22@gmail.com>
2024-04-15 14:43:39 +0800

    refactor: passport middleware.

--- server/src/middlewares/Passport.middleware.ts
+++ server/src/middlewares/Passport.middleware.ts
@@ -1,41 +1,47 @@
-import passport from "passport"
-import { Strategy as LocalStrategy } from "passport-local"
-import { database } from "../utils/database"
-import { generatePasswordHash } from "../utils/generatePasswordHash"
-import { User } from "../utils/types"
+import passport from 'passport'
+import { Strategy as LocalStrategy } from 'passport-local'
+import { database } from '../utils/database'
+import { generatePasswordHash } from '../utils/generatePasswordHash'
+import { User } from '../utils/types'
 
 passport.use(new LocalStrategy({
-    usernameField: 'emailAddress',
-    passwordField: 'password'
+  usernameField: 'emailAddress',
+  passwordField: 'password',
 }, async (emailAddress, password, done) => {
-    try {
-        const user = await database("users").where({ email_address: emailAddress }).first()
+  try {
+    const user = await database('users')
+      .where({ email_address: emailAddress })
+      .first()
 
-        const hashedPassword = await generatePasswordHash(password, user.password_salt)
+    const hashedPassword = await generatePasswordHash(password, user.password_salt)
 
-        if (hashedPassword !== user.password_hash || !user) {
-            return done(null, false, { message: 'Incorrect username or password.' })
-        }
-
-        return done(null, user)
-    } catch (error) {
-        return done(error)
+    if (hashedPassword !== user.password_hash || !user) {
+      return done(null, false, { message: 'Incorrect username or password.' })
     }
+
+    const { id, full_name, email_address, phone_number, is_admin } = user
+
+    return done(null, { id, full_name, email_address, phone_number, is_admin })
+  } catch (error) {
+    return done(error)
+  }
 }))
 
-passport.serializeUser((user, done) => {
-    done(null, user)
-});
+// @ts-ignore
+passport.serializeUser((user: User, done) => {
+  done(null, user.id)
+})
 
-passport.deserializeUser(async (user: User, done) => {
-    const { id} = user
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const user = await database('users').where({ id }).first()
 
-    try {
-        const user = await database("users").where({ id }).first()
-        done(null, user)
-    } catch (error) {
-        done(error)
-    }
+    const { id: userId, full_name, email_address, phone_number, is_admin } = user
+
+    done(null, { id: userId, full_name, email_address, phone_number, is_admin })
+  } catch (error) {
+    done(error)
+  }
 })
 
 export default passport
