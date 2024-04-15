4c703cedcac842ad442065ede53f91e23ac851da
dm-valdez <dmvdev.22@gmail.com>
2024-04-15 10:15:26 +0800

    chore: added validation form utility.

---
+++ client/src/lib/types.ts
@@ -0,0 +1,19 @@
+export interface FieldConfig {
+  value: string
+  rules: ValidationRule[]
+}
+
+export interface ValidationRule {
+  type: 'required' | 'email' | 'minLength'
+  message: string
+  minLength?: number
+}
+
+export interface LoginRequest {
+  emailAddress: string
+  password: string
+}
+
+export interface LoginResponse {
+  message: string
+}
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,45 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { FieldConfig } from './types.ts'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+function isValidEmail(email: string): boolean {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return emailRegex.test(email)
+}
+
+export function validateFields(fields: { [key: string]: FieldConfig }): { [key: string]: string } {
+  const errors: { [key: string]: string } = {}
+
+  Object.keys(fields).forEach((fieldName) => {
+    const value = fields[fieldName].value
+    const rules = fields[fieldName].rules
+
+    rules.forEach((rule) => {
+      switch (rule.type) {
+        case 'required':
+          if (!value.trim()) {
+            errors[fieldName] = rule.message
+          }
+          break
+        case 'email':
+          if (value.trim() && !isValidEmail(value)) {
+            errors[fieldName] = rule.message
+          }
+          break
+        case 'minLength':
+          if (value.trim() && rule.minLength && value.length < rule.minLength) {
+            errors[fieldName] = rule.message
+          }
+          break
+        // Add more validation rules as needed
+        default:
+          break
+      }
+    })
+  })
+  return errors
 }
