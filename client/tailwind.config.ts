7741ba1fb874dc2e8cc2d2ae65d41f8da8dd2eea
dm-valdez <dmvdev.22@gmail.com>
2024-04-16 07:01:33 +0800

    chore: added radix dialog.

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
         'option-4': '#403179',
         'option-5': '#52CBBB',
         'option-6': '#CC0000',
+        'option-7': '#FF3333',
       },
       boxShadow: {
         '3xl': '0 5px 55px -15px rgba(0, 0, 0, 0.3)'
